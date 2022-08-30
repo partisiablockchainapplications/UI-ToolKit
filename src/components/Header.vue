1e195c5712638af90d703c82ea8657f17cf0f48c
David Abimbola <abimbola130@gmail.com>
2022-08-30 08:23:46 -0500

    chore: create a stable version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,5 @@
 <template>
-  <header
-    style="z-index: 10"
-    class="bg-gray-50 px-5 py-5 lg:px-8 text-white border-b border-white sticky top-0"
-  >
+  <header style="z-index: 10" class="bg-gray-50 px-5 py-5 lg:px-8 text-white border-b border-white sticky top-0">
     <div class="flex items-center justify-between max-w-7xl m-auto">
       <div>
         <img src="../assets/charity.png" alt="Logo" class="w-[70px] h-[50px]" />
@@ -10,13 +7,20 @@
       <div class="flex flex-row">
         <a
           href="#"
+          @click="openDeployContract"
+          role="button"
+          class="block w-fit rounded-[100px] font-display font-bold text-white text-center text-lg lg:text-xl whitespace-nowrap sm:px-4 lg:px-8 py-4 ml-auto web3-button"
+        >
+          <span class="web3-span text-sm sm:text-lg text-black font-bold px-4 py-4 sm10">DEPLOY CONTRACT</span>
+        </a>
+
+        <a
+          href="#"
           @click="connectWallet"
           role="button"
           class="block w-fit bg-peace rounded-[100px] font-display font-bold text-white text-center text-lg lg:text-xl whitespace-nowrap sm:px-4 lg:px-8 py-4 ml-auto web3-button"
         >
-          <span class="web3-span text-sm sm:text-lg text-black font-bold px-4 py-4 sm10"
-            >CONNECT WALLET FOR PEACE</span
-          >
+          <span class="web3-span text-sm sm:text-lg text-black font-bold px-4 py-4 sm10">CONNECT PARTISIA WALLET</span>
         </a>
       </div>
     </div>
@@ -24,30 +28,34 @@
 </template>
 
 <script>
-import PartisiaSdk from "partisia-sdk";
+import PartisiaSdk from 'partisia-sdk'
 
 export default {
   data() {
     return {
       displayModal: false,
-    };
+    }
   },
 
   methods: {
-     async connectWallet() {
+    async connectWallet() {
       try {
-        const sdk = new PartisiaSdk();
+        const sdk = new PartisiaSdk()
         await sdk.connect({
-          chainId: "Partisia Blockchain",
-          permissions: ["sign"],
-          dappName: "PARTISIA UI toolkit",
-        });
+          chainId: 'Partisia Blockchain',
+          permissions: ['sign'],
+          dappName: 'PARTISIA UI toolkit',
+        })
       } catch (err) {
-        console.log(err.message);
+        console.log(err.message)
       }
     },
+
+    openDeployContract() {
+      window.open('https://mpcexplorer.com/deploy-contract', '_blank')
+    },
   },
-};
+}
 </script>
 
 <style scoped>
