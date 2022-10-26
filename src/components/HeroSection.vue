6cd0b69a51ac3c1c3bf7776aff4fde6d5abde91a
David Abimbola <abimbola130@gmail.com>
2022-10-26 11:31:08 -0500

    chore: changed PEACE text

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -12,15 +12,15 @@
         <h1
           class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
         >
-          <span class="block xl:inline">Welcome to PEACEDAO</span>
+          <span class="block xl:inline">Welcome to PARTISIA UI TOOLKIT</span>
         </h1>
         <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
             <a
-              href="http://empoweringpeace.net/"
+              href="https://partisiablockchain.com//"
               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
             >
-              Learn more about PEACE
+              Learn more about PARTISIA
             </a>
           </div>
         </div>
